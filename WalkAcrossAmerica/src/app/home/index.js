c44d9be88b742d48257ce704366dce170069dc37
Vallabhaneni <avallabhaneni@foliage.com>
2016-10-14 11:43:18 -0400

    changes in the maps

--- WalkAcrossAmerica/src/app/home/index.js
+++ WalkAcrossAmerica/src/app/home/index.js
@@ -46,6 +46,7 @@ export default class HomeCtrl {
         //initializing the values on the form
         $scope.init = function() {
             memberTeams();
+            teamLocations();
         };
 
 
@@ -140,14 +141,12 @@ export default class HomeCtrl {
                     $scope.firstname = '';
                     $scope.lastname = '';
                     $scope.emailaddress = '';
-
                 },
                 function(response){
                     $scope.message= 'Error in creating a new team member.'
                     $('#errorMessageDiv').show();
                 }
             );
-
         }
 
         //web service to add steps to the team
@@ -170,6 +169,9 @@ export default class HomeCtrl {
                 function(response){
                     $scope.message= 'Steps have been added to the member'
                     $('#successmessageDiv').show();
+                    $scope.steps = '';
+                    $scope.comments = '';
+                    teamLocations();
                 },
                 function(response){
                     $scope.message= 'Error in adding steps.'
@@ -177,6 +179,73 @@ export default class HomeCtrl {
                 }
             );
 
+        }
+
+        //function to plot the teams on the map based on the number of steps ccovered
+        var teamLocations = function () {
+            $http({
+                method : 'GET',
+                url : hostNameService.getHostName() + "/team/locations",
+                headers : {
+                    'Content-Type' : 'application/json',
+                }
+            }).then(
+                function(response){
+                    console.log("The team locations are:"+response);
+                    $scope.teamLocations = response.data;
+
+                    var mapOptions = {
+                        zoom: 5,
+                        center: new google.maps.LatLng(40.0000, -98.0000),
+                        mapTypeId: google.maps.MapTypeId.TERRAIN
+                    }
+
+                    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                    $scope.markers = [];
+
+                    var infoWindow = new google.maps.InfoWindow();
+                    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+                    var createMarker = function (info){
+                        var marker = new google.maps.Marker({
+                            map: $scope.map,
+                            position: new google.maps.LatLng(info.latitude, info.longitude),
+                            title: info.teamName,
+                            icon: {
+                                path: google.maps.SymbolPath.CIRCLE,
+                                strokeColor: info.teamColor,
+                                scale: 6
+                            },
+                        });
+                        marker.content = '<div class="infoWindowContent">' + "Total Steps:" +info.totalStepCount + '</div>';
+
+                        google.maps.event.addListener(marker, 'click', function(){
+                            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                            infoWindow.open($scope.map, marker);
+                        });
+                        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                        infoWindow.open($scope.map, marker);
+
+                        $scope.markers.push(marker);
+
+                    }
+
+                    for (var i = 0; i < $scope.teamLocations.length; i++){
+                        createMarker($scope.teamLocations[i]);
+                    }
+
+
+                    $scope.openInfoWindow = function(e, selectedMarker){
+                        e.preventDefault();
+                        google.maps.event.trigger(selectedMarker, 'click');
+                    }
+
+                },
+                function(response){
+
+                }
+            );
         }
 
         //refreshing the teams in the add members panel
@@ -217,46 +286,7 @@ export default class HomeCtrl {
             }
         ];
 
-        var mapOptions = {
-            zoom: 5,
-            center: new google.maps.LatLng(40.0000, -98.0000),
-            mapTypeId: google.maps.MapTypeId.TERRAIN
-        }
 
-        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        $scope.markers = [];
-
-        var infoWindow = new google.maps.InfoWindow();
-        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-
-        var createMarker = function (info){
-
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(info.lat, info.long),
-                title: info.city,
-                icon: info.image
-            });
-            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-
-            google.maps.event.addListener(marker, 'click', function(){
-                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-                infoWindow.open($scope.map, marker);
-            });
-
-            $scope.markers.push(marker);
-
-        }
-
-        for (var i = 0; i < cities.length; i++){
-            createMarker(cities[i]);
-        }
-
-        $scope.openInfoWindow = function(e, selectedMarker){
-            e.preventDefault();
-            google.maps.event.trigger(selectedMarker, 'click');
-        }
 
 
     }
